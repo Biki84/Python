d915ca8505637a8be5a5d2b40cbaf0c9b7ebf723
Bikash <96294096+Biki84@users.noreply.github.com>
2023-12-05 22:01:35 +0545

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,16 @@
+x = 5
+print(x)
+
+name = "Bikash"
+print(name)
+
+a = 4
+b = 5
+c = a+b
+print(c)
+
+
+d = float(input("Enter 1st Number"))
+e = float(input("Enter 2nd Number"))
+div = d / e
+print(div)
